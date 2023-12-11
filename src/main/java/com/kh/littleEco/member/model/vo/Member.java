051a723bbdbb82e3a90bd2c0331919f9a3d1b844
user1 <user1@DESKTOP-5RFL6US>
2023-12-11 19:26:02 +0900

    멤버 vo

---
+++ src/main/java/com/kh/littleEco/member/model/vo/Member.java
@@ -0,0 +1,25 @@
+package com.kh.littleEco.member.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Member {
+
+	private int memberNo;
+	private int categoryNo;
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private String nickName;
+	private String birth;
+	private String gender;
+	private String email;
+	private String phone;
+	private String createDate;
+	private String status;
+	
+}
