70a9a9bad6c3f909c67bab9bb7f8301ba719b6cd
user1 <user1@DESKTOP-5RFL6US>
2023-12-20 17:56:13 +0900

    pull conflit 해결

---
+++ src/main/java/com/kh/littleEco/member/model/vo/MemberCategory.java
@@ -0,0 +1,15 @@
+package com.kh.littleEco.member.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MemberCategory {
+	
+	private int categoryNo;
+	private String categoryName;
+
+}
