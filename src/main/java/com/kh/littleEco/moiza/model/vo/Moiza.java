83d9b2700bdf4bfd391543b45678f2e822b071de
miu1217 <hyun671217@daum.net>
2024-01-05 18:41:41 +0900

    moiza insert/list 기능 완성

--- src/main/java/com/kh/littleEco/moiza/model/vo/Moiza.java
+++ src/main/java/com/kh/littleEco/moiza/model/vo/Moiza.java
@@ -1,6 +1,9 @@
-package com.kh.littleEco.clud.model.vo;
+package com.kh.littleEco.moiza.model.vo;
 
 import java.sql.Date;
+import java.util.ArrayList;
+
+import com.kh.littleEco.brand.model.vo.Category;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,15 +12,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Club {
+public class Moiza {
 
-	private int clubNo;
-	private String clubName;
-	private String clubTitle;
-	private String clubGoal;
-	private String clubMission;
-	private String clubInfo;
+	private int moizaNo;
+	private ArrayList<Integer> categoryNo;
+	private String moizaName;
+	private String moizaTitle;
+	private String moizaMission;
+	private String moizaGoal;
+	private String moizaInfo;
 	private Date createDate;
 	private String status;
 	
+	
+	private ArrayList<Category>cList;
 }
--- src/main/java/com/kh/littleEco/moiza/model/vo/MoizaMember.java
+++ src/main/java/com/kh/littleEco/moiza/model/vo/MoizaMember.java
@@ -1,4 +1,4 @@
-package com.kh.littleEco.clud.model.vo;
+package com.kh.littleEco.moiza.model.vo;
 
 import java.sql.Date;
 
@@ -9,9 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class ClubMember {
+public class MoizaMember {
 
-	private int clubNo;
+	private int moizaNo;
 	private int memberNo;
 	private int memberLevel;
 	private int approvalStatus;
