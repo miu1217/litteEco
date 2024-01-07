17e4da9e427ab53a802d1ef0fb2f559c7bcffb05
miu1217 <hyun671217@daum.net>
2024-01-08 08:06:12 +0900

    모집게시판 crud

--- src/main/java/com/kh/littleEco/moiza/model/service/MoizaService.java
+++ src/main/java/com/kh/littleEco/moiza/model/service/MoizaService.java
@@ -3,20 +3,44 @@ package com.kh.littleEco.moiza.model.service;
 import java.util.ArrayList;
 
 import com.kh.littleEco.moiza.model.vo.Moiza;
+import com.kh.littleEco.moiza.model.vo.MoizaCategory;
+import com.kh.littleEco.moiza.model.vo.MoizaMember;
 
 public interface MoizaService {
 	
 	//모집 게시판 리스트 메소드
-	ArrayList<Moiza> MoizaList();
+	ArrayList<Moiza> selectMoizaList();
 	
 	//모집 게시판 insert 시 moiza 입력
-	int MoizaInsert(Moiza moiza);
+	int insertMoiza(Moiza moiza);
 
 	//모집 게시판 insert 시 생성자를 moizaMemebr 테이블에 입력 
-	int MoizaCreatorInsert(int memberNo);
+	int insertMoizaCreator(int memberNo);
 
 	//모집 게시판 category 입력
-	int MoizaCategory(Moiza moiza);
+	int insertMoizaCategory(Moiza moiza);
+
+	//모집 게시판 상세 페이지 조회
+	Moiza selectMoizaDetail(int mno);
+
+	//모집 게시판 상세 페이지 조회 시 카테고리 조회
+	ArrayList<MoizaCategory> selectMoizaCategory(int mno);
+
+	//모집 게시판 상세 페이지 생성자 조회
+	MoizaMember selectMoizaCreator(int mno);
+
+	//모집 게시판 업데이트 하는 메소드
+	int updateMoiza(Moiza moiza);
+
+	//모집 게시판 업데이트 시 카테고리 삭제
+	int deleteMoizaCategory(Moiza moiza);
+
+	//모집 게시판 업데이트 시 카테고리 새로 입력
+	int insertNewMoizaCategory(Moiza moiza);
+
+	//모집게시판 삭제
+	int deleteMoiza(int mno);
+
 
 
 }
