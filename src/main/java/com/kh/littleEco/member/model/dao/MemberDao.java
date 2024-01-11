27d1fcbd705afc3943302682f94d674a4d011f93
user1 <user1@DESKTOP-5RFL6US>
2024-01-11 11:00:26 +0900

    세춘이꺼 합친거

--- src/main/java/com/kh/littleEco/member/model/dao/MemberDao.java
+++ src/main/java/com/kh/littleEco/member/model/dao/MemberDao.java
@@ -9,54 +9,69 @@ import com.kh.littleEco.member.model.vo.Member;
 @Repository
 public class MemberDao {
 
-	//회원가입 메소드
-	public int insertMember(Member m,SqlSessionTemplate sqlSession) {
-		return sqlSession.insert("memberMapper.insertMember",m);
+	// 회원정보 수정
+	public int updateMember(SqlSessionTemplate sqlSession, Member loginUser) {
+		return sqlSession.update("memberMapper.updateMember", loginUser);
 	}
 
-	//로그인 메소드
+	// 비밀번호 변경
+	public int updateMember2(SqlSessionTemplate sqlSession, Member loginUser) {
+		return sqlSession.update("memberMapper.updatePwd", loginUser);
+	}
+
+	// 회원탈퇴
+	public int deleteMember(Member loginUser, SqlSessionTemplate sqlSession) {
+		return sqlSession.update("memberMapper.deleteMember", loginUser);
+	}
+
+	// 회원가입 메소드
+	public int insertMember(Member m, SqlSessionTemplate sqlSession) {
+		return sqlSession.insert("memberMapper.insertMember", m);
+	}
+
+	// 로그인 메소드
 	public Member loginMember(Member m, SqlSessionTemplate sqlSession) {
-		return sqlSession.selectOne("memberMapper.loginMember",m);
+		return sqlSession.selectOne("memberMapper.loginMember", m);
 	}
 
-	//멤버카테고리 삽입 메소드
+	// 멤버카테고리 삽입 메소드
 	public int insertMemberCategory(Member m, SqlSessionTemplate sqlSession) {
 		return sqlSession.insert("memberMapper.insertMemberCategory", m);
 	}
 
-	//id 찾기
+	// id 찾기
 	public Member selectFindId(Member m, SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("memberMapper.selectFindId", m);
 	}
 
-	//pwd 찾기(확인)
+	// pwd 찾기(확인)
 	public Member selectFindPwd(Member m, SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("memberMapper.selectFindMember", m);
 	}
 
-	//pwd 새로입력
+	// pwd 새로입력
 	public int resetMemberPwd(Member m, SqlSessionTemplate sqlSession) {
 		return sqlSession.update("memberMapper.resetMemberPwd", m);
 	}
 
-	//아이디중복확인
+	// 아이디중복확인
 	public int checkId(String memberId, SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("memberMapper.checkId", memberId);
 	}
 
-	//닉네임중복확인
+	// 닉네임중복확인
 	public int checkNick(String nickName, SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("memberMapper.checkNick", nickName);
 	}
 
-	//이메일중복확인
+	// 이메일중복확인
 	public int checkEmail(String email, SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("memberMapper.checkEmail", email);
 	}
 
-	//폰번호 중복확인
+	// 폰번호 중복확인
 	public int checkPhone(String phone, SqlSessionTemplate sqlSession) {
-		return sqlSession.selectOne("memberMapper.checkPhone",phone);
+		return sqlSession.selectOne("memberMapper.checkPhone", phone);
 	}
-	
+
 }
