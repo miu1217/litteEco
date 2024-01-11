7ddcdb64f6b8040403ad5b48147f8761ef1fe263
user1 <user1@DESKTOP-5RFL6US>
2024-01-11 14:28:02 +0900

    공백 주석 제거

--- src/main/java/com/kh/littleEco/searchEco/controller/SearchEcoController.java
+++ src/main/java/com/kh/littleEco/searchEco/controller/SearchEcoController.java
@@ -38,7 +38,6 @@ public class SearchEcoController {
 	@Autowired
 	SearchEcoService searchEcoService;
 	
-	
 	//서치에코 리스트 뷰
 	@RequestMapping("searchEco.bo")
 	public String searchEcoList(@RequestParam(value = "currentPage", defaultValue="1")int currentPage, Model model) {
@@ -50,9 +49,7 @@ public class SearchEcoController {
 		int pageLimit = 10;
 		
 		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, pageLimit, boardLimit);
-		
-		//System.out.println(currentPage);
-		
+
 		ArrayList<SearchEco> sList = searchEcoService.searchEcoList(pi);
 		
 		model.addAttribute("sList",sList);
@@ -63,59 +60,6 @@ public class SearchEcoController {
 		
 	}
 	
-	/*
-	 * //좋아요 인서트 기능 관련
-	 * 
-	 * @PostMapping("likeInsert.bo") public int ecoLikeInsert(int bno, int memberNo)
-	 * {
-	 * 
-	 * System.out.println("LIKEINSERT" + bno + memberNo);
-	 * 
-	 * int num = 0;
-	 * 
-	 * // 동일 게시글에 대한 이전 추천 여부 검색 int result = searchEcoService.ecoLikeCheck(bno,
-	 * memberNo);
-	 * 
-	 * if(result == 0){ // 추천하지 않았다면 추천 추가 num = searchEcoService.ecoLikeUpdate(bno,
-	 * memberNo); }else{ // 추천을 하였다면 추천 삭제 num = searchEcoService.ecoLikeDelete(bno,
-	 * memberNo); }
-	 * 
-	 * return searchEcoService.ecoLikeInsert(bno, memberNo);
-	 * 
-	 * }
-	 */
-	
-	/*
-	 * //좋아요 카운트 기능 관련
-	 * 
-	 * @ResponseBody
-	 * 
-	 * @PostMapping(value = "likeCount.bo", produces =
-	 * "application/json; charset=UTF-8") public int ecoLikeCount(int bno) {
-	 * 
-	 * return searchEcoService.ecoLikeCount(bno);
-	 * 
-	 * }
-	 * 
-	 * //좋아요 체크 기능 관련
-	 * 
-	 * @ResponseBody
-	 * 
-	 * @PostMapping(value = "likeCheck.bo", produces =
-	 * "application/json; charset=UTF-8") public int ecoLikeCheck(int bno, int
-	 * memberNo) {
-	 * 
-	 * System.out.println("일로왔나"); System.out.println(bno);
-	 * System.out.println(memberNo);
-	 * 
-	 * return searchEcoService.likeMemCheck(bno, memberNo);
-	 * 
-	 * // JSON 형식으로 응답 // Gson gson = new Gson(); // Google Gson 라이브러리를 사용하여 JSON 변환
-	 * // String json = gson.toJson(result); // // return
-	 * searchEcoService.ecoLikeCheck(bno, memberNo);
-	 * 
-	 * }
-	 */
 	//좋아요 인서트
 	@ResponseBody
 	@RequestMapping("likeInsert.bo")
@@ -146,131 +90,10 @@ public class SearchEcoController {
 		return e;
 	}
 	
-	
-	
-	
 	@GetMapping("searchEcoInsert.bo")
 	public String seInsert() {
 		return "searchEco/searchEcoEnrollForm";
 	}
-	/*
-   @PostMapping("searchEcoInsert.bo")
-   public void InsertEco(SearchEco se
-		   					,Attachment at
-		   					,HttpServletRequest request
-		   					,HttpServletResponse response
-		   					,HttpSession session
-		   					) throws IOException {
-	   
-	   
-	   
-	   
-		if(ServletFileUpload.isMultipartContent(request)) {
-			
-			int maxSize = 10 * 1024 * 1024;
-			
-			//저장경로
-			String savePath = request.getSession().getServletContext().getRealPath("/resources/uploadFiles/");
-			MultipartRequest multiRequest = new MultipartRequest(request,savePath,maxSize,"URF-8",new MyFileRenamePolicy());
-			
-			
-			String title = request.getParameter("searchEcoTitle");
-			String content = request.getParameter("searchEcoBoardContent");
-			
-			se.setEcoBoardContent(content);
-			se.setEcoBoardTitle(title);
-			
-			ArrayList<Attachment> list = new ArrayList<>();
-			
-			//각 키값을 반복 돌리면서 요소 꺼내주기
-			
-			for(int i=1; i<=3; i++) {
-				//키값
-				String key = "file"+i;
-				
-				//키값에 해당하는 요소가 있는지 확인하기
-				if(multiRequest.getOriginalFileName(key)!=null) {
-					//해당 키값에 파일이 있다면
-					//Attachment 객체 생성후 데이터 담아주기
-					//여러개가 나올 수 있으니 Attachment객체를 list에 추가하기
-					//원본명,변경이름,파일경로,파일레벨(썸네일사진/상세사진)
-					
-					at.setOriginName(multiRequest.getOriginalFileName(key));
-					at.setChangeName(multiRequest.getFilesystemName(key));
-					at.setFilePath("/resources/uploadFiles/");
-					
-					if(i==1) { //대표이미지 fileLevel == 1
-						at.setFileLevel(1);
-					}else { //상세이미지 fileLevel == 2
-						at.setFileLevel(2);
-					}
-					list.add(at); //값 추출 끝났으니 리스트에 추가하기.
-				}
-			}
-			//서비스에 요청하기
-			
-			int result = searchEcoService.searchEcoinsert(se,list);
-			HttpSession session1 = request.getSession();
-			
-			if(result>0) { //성공
-				//성공메세지와함께 사진게시판 목록 보여주기
-				session1.setAttribute("alertMsg", "게시글을 등록했습니다.");
-				response.sendRedirect(request.getContextPath()+"/searchEco.bo");
-				
-				
-			}else {//실패
-				//실패메세지와함께 사진게시판 목록 보여주기
-				session1.setAttribute("alertMsg", "게시글 작성을 실패했습니다.");
-				response.sendRedirect(request.getContextPath()+"/searchEco.bo");
-				
-			}
-		}
-	   
-	   
-   }
-
-	
-	
-	@PostMapping("searchEcoInsert.bo")
-	public String insertSearchEco(SearchEco se,Model model,MultipartFile upFile,HttpSession session) {
-		
-		
-		int presult =searchEcoService.insertSearchEco(se);
-		int aresult = 0;
-		
-		System.out.println("등록p : "+se);
-		
-		if(!upFile.getOriginalFilename().equals("")) {
-			
-			Attachment a = new Attachment();
-			
-			String changeName = saveFile(upFile,session);
-			
-			a.setOriginName(upFile.getOriginalFilename());
-			a.setChangeName(changeName);
-			a.setFilePath("resources/uploadFiles/");
-			
-			aresult = searchEcoService.insertAttachment(a);
-			
-			System.out.println("등록pr : "+presult);
-			System.out.println("등록ar : "+aresult);
-			System.out.println("등록a : "+a);
-			
-		}
-			
-		
-			if((presult*aresult)>0) {
-				
-				session.setAttribute("alertMsg", "게시글 등록 성공");
-				return "redirect:searchEco.bo";
-				
-				}else {
-					session.setAttribute("alertMsg", "게시글 등록 실패");
-					return "common/errorPage";
-			}
-		
-	}
-	*/
 	
 	@PostMapping("searchEcoInsert.bo")
 	public String insertSearchEco(SearchEco se, Brand b, Attachment at, Model model,
@@ -296,25 +119,6 @@ public class SearchEcoController {
 		//se.setThumImg(upFile.getOriginalFilename());
 		
 		int result2 = searchEcoService.insertSearchEco(se);
-		
-		//upFileDetail
-		
-		//파일 체크해서 Attachment 테이블에 인서트(썸네일)
-		/*
-		 * if(!upFile.getOriginalFilename().equals("")) {
-		 * 
-		 * 
-		 * 
-		 * String changeName = saveFile(upFile, session);
-		 * 
-		 * at.setFileNo(fileNo); at.setOriginName(upFile.getOriginalFilename());
-		 * at.setChangeName(changeName); at.setFilePath("resources/brandAttachmet/");
-		 * 
-		 * at.setBrandNo(brandNo);
-		 * 
-		 * int result3 = searchEcoService.insertAttachment(at); }
-		 */
-
 		
 		if(result2 > 0 ) {
 			session.setAttribute("alertMsg", "브랜드 등록 성공");
@@ -439,15 +243,7 @@ public class SearchEcoController {
 		return "searchEco/searchEcoListView";
 
 	}
-	
 
-//	@RequestMapping("likeCheck")
-//	public String likeCheck(@RequestParam("ebno") int ecoBoardNo) {
-//		int bno;
-//		
-//		return null;
-//	}
-	
 	//메인에서 브랜드 클릭시 관련 보드번호 가져오기
 	@ResponseBody
 	@PostMapping("brandDetail.bo")
